7dd12def29f73d4017bd2ed7ca43d2aa1ff27719
A I <62871489+aliffirfan4523@users.noreply.github.com>
2025-12-06 23:44:45 +0800

    Implement localization support by adding ARB files and localization classes

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:padizdoctor/l10n/app_localizations.dart';
 
 import 'core/app_fonts.dart';
 import 'homepage/homepage_screen.dart';
